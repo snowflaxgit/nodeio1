c904e67f0216bc4c9f0aaed46c4097bda2a5d2f6
snowflaxgit <android@snowflax.com>
2013-06-14 19:08:43 +0530

    created krunal

---
+++ test/processor.test.js
@@ -0,0 +1,469 @@
+var nodeio = require('../lib/node.io'),
+    utils = require('../lib/node.io/utils'),
+    processor = new nodeio.Processor(),
+    assert = require('assert');
+
+function createJob(options, methods) {
+    if (typeof methods === 'undefined') {
+        methods = options;
+        options = {};
+    }
+    return new nodeio.Job(options, methods);
+}
+
+function startJob(job, callback, capture_output) {
+    nodeio.start(job, {silent:true}, callback, capture_output);
+}
+
+module.exports = {
+
+    'test running an empty job': function() {
+        //Prevent the job from looking for STDIN input
+        var job = createJob({once:true},{input:false});
+
+        startJob(job, function(err) {
+            assert.isUndefined(err);
+        });
+    },
+
+    'test running a basic job': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            output: function(output) {
+                output.forEach(function (num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,1,2]', JSON.stringify(out));
+        });
+    },
+
+    'test capturing output': function() {
+        var job = createJob({
+            input: [0,1,2]
+        });
+
+        startJob(job, function(err, out) {
+            assert.equal('[0,1,2]', JSON.stringify(out));
+        }, true);
+    },
+
+    'test emit() async': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                var self = this;
+                utils.tick(function() {
+                    self.emit(1);
+                });
+            },
+            output: function(output) {
+                output.forEach(function (num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[1,1,1]', JSON.stringify(out));
+        });
+    },
+
+    'test emitting via return': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                return 1;
+            },
+            output: function(output) {
+                output.forEach(function (num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[1,1,1]', JSON.stringify(out));
+        });
+    },
+
+    'test skipping on run': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                this.skip();
+            },
+            output: function(output) {
+                output.forEach(function (num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[]', JSON.stringify(out));
+        });
+    },
+
+    'test reduce()': function() {
+        var total = 0;
+
+        var job = createJob({
+            input: [0,1,2],
+            reduce: function(num) {
+                total += num[0];
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal(3, total);
+        });
+    },
+
+    'test reduce() emitting': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            reduce: function(num) {
+                this.emit(num[0] * 2);
+            },
+            output: function(num) {
+                out.push(num);
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,2,4]', JSON.stringify(out));
+        });
+    },
+
+    'test reduce() emitting via return': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            reduce: function(num) {
+                return num[0]*2;
+            },
+            output: function(num) {
+                out.push(num);
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,2,4]', JSON.stringify(out));
+        });
+    },
+
+    'test job fail()': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                this.fail(0);
+            },
+            fail: function(num) {
+                this.emit(num);
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,0,0]', JSON.stringify(out));
+        });
+    },
+
+    'test job fail() emitting via return': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                this.fail(0);
+            },
+            fail: function(num) {
+                return num;
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,0,0]', JSON.stringify(out));
+        });
+    },
+
+    'test fail() being called on validation error': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                try {
+                    this.assert('abc').isInt();
+                    return 1;
+                } catch (e) {
+                    return 0;
+                }
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,0,0]', JSON.stringify(out));
+        });
+    },
+
+    'test fail() being called on dom select error': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function() {
+                var self = this;
+                this.parseHtml('<p class="a"></p>', function(err, $) {
+                    if (err) return self.fail();
+                    assert.equal('a', $('p').attribs['class']);
+                    assert.throws(function () {
+                        $('#doesntexist')
+                    });
+                });
+            },
+            fail: function() {
+                return 0;
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,0,0]', JSON.stringify(out));
+        });
+    },
+
+    'test timeout during run()': function() {
+        var out = [];
+
+        var job = createJob({timeout: 0.1},{
+            input: [0,1,2],
+            run: function() {
+                var self = this;
+                setTimeout(function() {
+                    self.emit(1);
+                }, 2000);
+            },
+            fail: function(num, status) {
+                assert.equal('timeout', status);
+                this.emit(0);
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(err) {
+            assert.equal('[0,0,0]', JSON.stringify(out));
+        });
+    },
+
+    'test take op': function() {
+        var job = createJob({take:3},{
+            input: [0,1,2],
+            run: function(data) {
+                assert.equal('[0,1,2]', JSON.stringify(data));
+                this.skip();
+            }
+        });
+
+        startJob(job, function(){});
+    },
+
+    'test max op': function() {
+        var out = [];
+
+        var i = 0;
+
+        var job = createJob({max:3},{
+            input: [0,1,2],
+            run: function(num) {
+                var self = this;
+                i++;
+                process.nextTick(function() {
+                    assert.equal(3, i);
+                    self.skip();
+                });
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(){});
+    },
+
+    'test max op': function() {
+        var out = [];
+
+        var i = 0;
+
+        var job = createJob({retries:3},{
+            input: [0],
+            run: function(num) {
+                i++;
+                this.retry(num);
+            },
+            fail: function(num, status) {
+                assert.equal('retry',status);
+                this.emit('0 failed');
+            },
+            output: function(output) {
+                assert.equal('0 failed', output);
+                assert.equal(4, i); //First time + 3 retries = 4
+            }
+        });
+
+        startJob(job, function(){});
+    },
+
+    'test output with flattening': function() {
+        var out = [];
+
+        var job = createJob({
+            input: [0,1,2],
+            run: function(num) {
+                return [0,1];
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(){
+            assert.equal('[0,1,0,1,0,1]', JSON.stringify(out));
+        });
+    },
+
+    'test output without flattening': function() {
+        var out = [];
+
+        var job = createJob({flatten:false}, {
+            input: [0,1,2],
+            run: function(num) {
+                return [0,1];
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(){
+            assert.equal('[[0,1],[0,1],[0,1]]', JSON.stringify(out));
+        });
+    },
+
+    'test input op': function() {
+        var i = 0;
+
+        var job = createJob({input:10},{
+            input: true,
+            run: function() {
+                i++;
+                this.skip();
+            }
+        });
+
+        startJob(job, function(){
+            assert.equal(10, i);
+        });
+    },
+
+    'test adding input outside of job.input()': function() {
+        var i = 0, out = [];
+
+        var job = createJob({input:10},{
+            input: [0,1,2],
+            run: function(num) {
+                if (++i <= 3) {
+                    this.add(3);
+                };
+                this.emit(num);
+            },
+            output: function(output) {
+                output.forEach(function(num) {
+                    out.push(num);
+                });
+            }
+        });
+
+        startJob(job, function(){
+            assert.equal('[0,3,3,3,1,2]', JSON.stringify(out));
+        });
+    },
+
+    'test global timeout': function() {
+        var out = [];
+
+        var job = createJob({global_timeout:0.2}, {
+            input: [0,1,2],
+            run: function(num) {
+                setTimeout(function() {
+                    job.emit(num);
+                }, 1000);
+            },
+            output: false
+        });
+
+        startJob(job, function(err) {
+            assert.ok(err);
+        });
+    },
+
+    'test the recurse op': function() {
+        //Test specialised case where input is a path to a dir
+        var job = createJob({recurse:true}, {input:__dirname+'/resources/test_dir_input'});
+
+        startJob(job, function(err, files) {
+            assert.equal(4, files.length);
+            assert.ok(files.indexOf(__dirname+'/resources/test_dir_input/file.a') >= 0);
+            assert.ok(files.indexOf(__dirname+'/resources/test_dir_input/file.b') >= 0);
+            assert.ok(files.indexOf(__dirname+'/resources/test_dir_input/file.c') >= 0);
+            assert.ok(files.indexOf(__dirname+'/resources/test_dir_input/dir/file.d') >= 0);
+        }, true);
+    },
+
+}
---
+++ test/request.test.js
@@ -0,0 +1,410 @@
+var nodeio = require('../lib/node.io'),
+    http = require('http'),
+    assert = require('assert');
+
+var port = 24510, timeout = 2000;
+
+function createJob() {
+    var JobClass = nodeio.JobClass, job = new JobClass();
+    job.debug = function () {};
+    //Throw a warning on ECONNREFUSED rather than fail the entire test suite
+    job.fail = function (input, status) {
+        if (status === 'ECONNREFUSED') {
+            console.log('\x1B[33mWARNING\x1B[0m: \x1B[31mECONNREFUSED\x1B[0m (see request.test.js)');
+        }
+    };
+    return job;
+}
+
+function close (server) {
+    try {
+        server.close();
+    } catch (e) {}
+}
+
+module.exports = {
+
+    'test GET request': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Hello World');
+        });
+
+        server.listen(++port);
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            if (err) throw err;
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('Hello World', data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with custom headers': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            if (req.headers.foo === 'bar') {
+                res.writeHead(200,{'Content-Type': 'text/plain'});
+                res.end('Headers ok');
+            } else {
+                res.end();
+            }
+        });
+
+        server.listen(++port);
+
+        job.get('http://127.0.0.1:'+port+'/', {foo:'bar'}, function(err, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('Headers ok', data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with pre-parse callback': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(200,{'Content-Type': 'text/plain'});
+            res.end('&gt;&lt;');
+        });
+
+        server.listen(++port);
+
+        var parse = function(str) {
+            return str.replace('&gt;','>').replace('&lt;','<');
+        }
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('><', data);
+            close(server);
+        }, parse);
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test POST request': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            var data = '';
+            req.setEncoding('utf8');
+            req.addListener('data', function(chunk) { data += chunk; });
+            req.addListener('end', function() {
+                if (data === 'foo=bar') {
+                    res.writeHead(200,{'Content-Type': 'text/plain'});
+                    res.end('Post ok');
+                } else {
+                    res.end();
+                }
+            });
+        });
+
+        server.listen(++port);
+
+        job.post('http://127.0.0.1:'+port+'/', {foo:'bar'}, function(err, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('Post ok', data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request returning the dom': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(200,{'Content-Type': 'text/plain'});
+            res.end('<p class="a"></p>');
+        });
+
+        server.listen(++port);
+
+        job.getHtml('http://127.0.0.1:'+port+'/', function(err, $, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('<p class="a"></p>', data);
+            assert.equal('a', $('p').attribs['class']);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test POST request returning the dom': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            var data = '';
+            req.setEncoding('utf8');
+            req.addListener('data', function(chunk) { data += chunk; });
+            req.addListener('end', function() {
+                if (data === 'foo=bar') {
+                    res.writeHead(200,{'Content-Type': 'text/plain'});
+                    res.end('<p class="a"></p>');
+                } else {
+                    res.end();
+                }
+            });
+        });
+
+        server.listen(++port);
+
+        job.postHtml('http://127.0.0.1:'+port+'/', {foo:'bar'}, function(err, $, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('<p class="a"></p>', data);
+            assert.equal('a', $('p').attribs['class']);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test nested request': function() {
+
+        var p = ++port, job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            if (!req.headers.cookie) {
+                res.writeHead(200, {'Content-Type': 'text/plain', 'Set-Cookie': 'foo=bar'});
+                res.end('Ok');
+            } else if (req.headers.cookie === 'foo=bar' && req.headers.referer === 'http://127.0.0.1:'+p+'/') {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Ok2');
+            } else {
+                res.end();
+            }
+        });
+
+        server.listen(p);
+
+        job.get('http://127.0.0.1:'+p+'/', function(err, data, headers) {
+            assert.equal('Ok', data);
+            job.get('http://127.0.0.1:'+p+'/', function(err, data, headers) {
+                assert.equal('Ok2', data);
+                close(server);
+            });
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with custom headers 2': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            if (req.headers.foo === 'bar' && req.headers.cookie === 'coo=kie' && req.headers['user-agent'] === 'Firefox') {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Headers ok');
+            } else {
+                res.end();
+            }
+        });
+
+        server.listen(++port);
+
+        job.setHeader('foo', 'bar');
+        job.setCookie('coo', 'kie');
+        job.setUserAgent('Firefox');
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('Headers ok', data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with addCookie': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            var cookies = req.headers.cookie.split('; ');
+            if (cookies[0] === 'coo=kie' && cookies[1] === 'foo=bar') {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Headers ok');
+            } else {
+                res.end();
+            }
+        });
+
+        server.listen(++port);
+
+        job.addCookie('coo', 'kie');
+        job.addCookie('foo', 'bar');
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal('text/plain', headers['content-type']);
+            assert.equal('Headers ok', data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with 4xx response': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Forbidden');
+        });
+
+        server.listen(++port);
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal(400, err);
+            assert.equal(null, data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with 5xx response': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+        });
+
+        server.listen(++port);
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal(500, err);
+            assert.equal(null, data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test GET request with too many redirects': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(302, {'Content-Type': 'text/plain', 'Location': '/'});
+            res.end();
+        });
+
+        server.listen(++port);
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal('redirects', err);
+            assert.equal(null, data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+
+    'test POST request with too many redirects': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.writeHead(302, {'Content-Type': 'text/plain', 'Location': '/'});
+            res.end();
+        });
+
+        server.listen(++port);
+
+        job.post('http://127.0.0.1:'+port+'/', {foo:'bar'}, function(err, data, headers) {
+            assert.equal('redirects', err);
+            assert.equal(null, data);
+            close(server);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+
+    'test get() is in same scope as job': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.end();
+        });
+
+        server.listen(++port);
+
+        job.foo = 'bar';
+
+        job.get('http://127.0.0.1:'+port+'/', function(err, data, headers) {
+            assert.equal('bar', this.foo);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+
+    'test getHtml() is in same scope as job': function() {
+
+        var job = createJob();
+
+        var server = http.createServer(function (req, res) {
+            res.end();
+        });
+
+        server.listen(++port);
+
+        job.foo = 'bar';
+
+        job.getHtml('http://127.0.0.1:'+port+'/', function(err, $) {
+            assert.equal('bar', this.foo);
+        });
+
+        setTimeout(function() {
+            close(server);
+        }, timeout);
+    },
+}
